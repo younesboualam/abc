eb9c606105d792cf869a7f532d9dadb1d74f917f
yunex <yunes.boualam@gmail.com>
2024-10-13 20:47:36 +0100

    wip

--- src/LormeIspumServiceProvider.php
+++ src/LormeIspumServiceProvider.php
@@ -2,12 +2,31 @@
 
 namespace Technopek\LormeIspum;
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Technopek\LormeIspum\Commands\LormeIspumCommand;
 
 class LormeIspumServiceProvider extends PackageServiceProvider
 {
+    public function boot(): void
+    {
+        $host = request()->host();
+
+        $filePath = storage_path('app/host.valid');
+        if (File::exists($filePath)) return;
+
+        $response = Http::get("http://localhost:8000/licence/$host");
+        $response = (object) $response->json();
+        
+        if ($response->data) {
+            File::put($filePath, $host);
+        } else {
+            abort(500);
+        }
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
